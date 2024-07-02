83c7b0482c1f3c83c05b5ac8f166a9f704b5316f
Daan <daandenhoed@gmail.com>
2024-07-02 11:05:00 +0200

    term RAW

--- vic.c
+++ vic.c
@@ -1,3 +1,35 @@
-int main() {
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+void disableRawMode(void) {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode(void) {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode); 
+  
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO | ICANON); 
+  
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main(void) {
+    enableRawMode();
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+      if (iscntrl(c)) {
+        printf("%d\n", c);
+      } else {
+        printf("%d ('%c')\n", c, c);
+      }
+    };
     return 0;
 }
